3ccc2466a201145ce0164b1c6a5a0136b817c924
Lorcan <67399230+L0raxeo@users.noreply.github.com>
2021-10-21 18:41:24 -0400

    replace plugin List with plugin HashMap with processes

--- src/main/java/mc/plugins/PluginManager.java
+++ src/main/java/mc/plugins/PluginManager.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 
 public class PluginManager
 {
-
+    /**
+     * HashMap with all Plugins and associated processes
+     */
     public static HashMap<File, Process> allPlugins = new HashMap<>();
 
 }
