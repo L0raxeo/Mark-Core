4d451791e2f1475bb7672c142fc6db5e3db0fa08
Lorcan <67399230+L0raxeo@users.noreply.github.com>
2021-10-21 11:21:35 -0400

    update documentation

--- src/main/java/mc/init/Launcher.java
+++ src/main/java/mc/init/Launcher.java
@@ -2,8 +2,12 @@ package mc.init;
 
 import mc.MarkCore;
 
+/**
+ * M.A.R.K. origin
+ */
 public class Launcher
 {
+
     public static void main(String[] args)
     {
         MarkCore core = new MarkCore();
--- src/main/java/mc/init/PluginRegistry.java
+++ src/main/java/mc/init/PluginRegistry.java
@@ -15,10 +15,15 @@ public class PluginRegistry
      * Executes each plugin
      */
     public static void init() throws IOException {
+        // Iterates through all registered plugins
         for (File f : PluginManager.allPlugins)
         {
+            // Process builder buffers each process by building each step, and once that process is determined, it is executed
+            // 3 args: java -jar currentPlugin.jar
             ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", f.getName());
+            // Sets preferred working directory
             processBuilder.directory(new File("plugins"));
+            // Builds/executes process builder
             processBuilder.start();
 
             System.out.println("[INFO]: successfully built [" + f.getName() + "]");
@@ -32,6 +37,7 @@ public class PluginRegistry
     {
         File[] files = new File("plugins").listFiles();
 
+        // only iterates if file array is not null
         assert files != null;
         for (File file : files)
         {
