186be7dcc171f39e9d44f370827f2e5efdeec90a
L0raxeo <67399230+L0raxeo@users.noreply.github.com>
2021-11-02 17:19:05 -0400

    pre-loaded custom responses

--- src/main/java/com/arkicore/mark/core/init/IOFileRegistry.java
+++ src/main/java/com/arkicore/mark/core/init/IOFileRegistry.java
@@ -14,7 +14,7 @@ import java.io.IOException;
  * @author Lorcan Andrew Cheng
  */
 @VersionInfo(
-        version = "1.0",
+        version = "3.0",
         releaseDate = "10/21/2021",
         since = "1.0",
         contributors = {
@@ -44,7 +44,15 @@ public class IOFileRegistry implements Registry
 
         if (!new File("customResponses.txt").exists())
         {
-            FileLoader.writeFile("customResponses.txt", "");
+            FileLoader.writeFile("customResponses.txt", "hello=Hello!",
+                                                                        "hi=Hello!",
+                                                                        "who:[1w]:=:[search]:",
+                                                                        "what:[1w]:=:[search]:",
+                                                                        "where:[1w]:=:[search]:",
+                                                                        "when:[1w]:=:[search]:",
+                                                                        "why:[1w]:=:[search]:",
+                                                                        "how:[1w]:=:[search]:",
+                                                                        "when were you born=September 24, 2021");
         }
     }
 
